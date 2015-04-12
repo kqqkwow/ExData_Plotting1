1d1b9b42ba7054b7ad0800d78db2f90bfb0e4531
kqqkwow <kqqkwow@hotmail.com>
2015-04-12 18:34:53 +0800

    Signed-off-by: kqqkwow <kqqkwow@hotmail.com>

--- plot4.R
+++ plot4.R
@@ -10,6 +10,6 @@ plot(newdata$Time,newdata$Voltage,type="l",ylab="Voltage",xlab="datetime",ylim=c
 plot(newdata$Time,newdata$Sub_metering_1,type="l",col="black",xlab="",ylab="Energy sub metering")
 lines(newdata$Time,newdata$Sub_metering_2,type="l",col="red")
 lines(newdata$Time,newdata$Sub_metering_3,type="l",col="blue")
-legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("black","red","blue"))
+legend("topright",legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("black","red","blue"),bty="n")
 plot(newdata$Time,newdata$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
 dev.off()
